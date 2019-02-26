61ae40a6d35e8b4eb78261165c73dfc76272064d
eizocompiretion <47878917+eizocompiretion@users.noreply.github.com>
2019-02-26 19:31:19 +0900

    Add files via upload
    [add]

---
+++ docs/assets/js/index.js
@@ -0,0 +1,7 @@
+var animationTivel = lottie.loadAnimation({
+  container: document.getElementById('tivel'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'assets/json/data.json'
+});
