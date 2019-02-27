be76cb180d35bfcbbef31f3eb2d658b520aa1e3f
eizocompiretion <47878917+eizocompiretion@users.noreply.github.com>
2019-02-27 13:34:12 +0900

    Add files via upload

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -5,14 +5,3 @@ var animationTivel = lottie.loadAnimation({
   autoplay: true,
   path: 'assets/json/data.json'
 });
-
-$(function() {
-    var h = $(window).height(); // ブラウザウィンドウの高さを取得する
-    $('#contents').css('display','none'); // コンテンツを非表示にする
-    $('#loader-bg ,#loader').height(h).css('display','block');//ローディング画像を表示
-});
-$(window).on('load', function () { // 読み込み完了したら実行する
-    $('#loader-bg').delay(900).fadeOut(800);// ローディングを隠す
-    $('#loader').delay(600).fadeOut(300);
-    $('#contens').css('display', 'block');// コンテンツを表示する
-});
